ad7541d02994aca23914740b5ffbca7bf264e1e6
Antoine Leclair <antoineleclair@gmail.com>
2020-06-29 17:12:31 -0400

    Move things around

--- demobuildissue/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from bottle import route, run, template
-
-@route('/')
-def index(name):
-    return template('<h1>Hi!</h1>')
-
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
